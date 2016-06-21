4c3af7dcae32b731db0df2c7e7bab4f91e7be2dd
kislay kumar <workmail.raj@gmail.com>
2016-06-21 21:51:34 +0530

    Update dbmanger.php

--- system/engine/dbmanger.php
+++ system/engine/dbmanger.php
@@ -1,10 +1,10 @@
 <?php
  class dbmanger{
 	protected $con=NULL;
-	private $db=array('HOSTNAME'=>'localhost','USER'=>'root','PASS'=>'password','DB'=>'robotic','driver'=>"mysqli");
+	private $db=array('HOSTNAME'=>'localhost','USER'=>'root','PASS'=>'','DB'=>'skelvescrm','driver'=>"dmysqli");
 	function getdb(){
 		include_once Db.$this->db['driver'].'.php';
-	return new $this->db['driver']($this->db);
+	return new  $this->db['driver']($this->db);
 	}
 	function __autoload($class_name) {
 	if(is_file(db.$class_name.'.php')){ include_once DB.$class_name.'.php'; } 	
